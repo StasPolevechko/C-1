2877b33ee1c086e13da159f32865aa5173a6e6d6
Stas <staspolevechko@gmail.com>
2023-04-11 22:42:19 +0300

    задача 1  готова

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,4 +1,15 @@
-﻿Console.Write("введите целое число: ");
-string userInput = Console.ReadLine() ?? "";
+﻿Console.Write("введите первое целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+Console.Write("введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
 
-Console.WriteLine(userInput);
+int userNamber1 = int.Parse(userInput1);
+int userNamber2 = int.Parse(userInput2);
+if (userNamber1 == userNamber2 * userNamber2)
+{
+    Console.WriteLine($"a={userNamber1}, b={userInput2} -> да");
+}
+else
+{
+    Console.WriteLine($"a={userNamber1}, b={userInput2} -> нет");
+};
